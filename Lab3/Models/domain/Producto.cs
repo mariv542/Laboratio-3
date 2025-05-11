cf475200c2252fb606e480a538bdfe4d4e964269
unknown <Hilary V>
2025-05-11 13:58:08 -0400

    First commit

---
+++ Lab3/Models/domain/Producto.cs
@@ -0,0 +1,20 @@
+﻿namespace Lab3.Models.domain
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+
+        public Producto() { 
+        }
+        
+        public Producto (string name, string description)
+        {
+            Name = name;
+            Description = description;
+
+        }
+    }
+}
